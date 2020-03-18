bebfb65b36224eee2214e54e487752b2eeb37dbb
valooford <vladan.vip.vladan@gmail.com>
2020-03-18 16:52:58 +0300

    Updates

---
+++ precache-manifest.539ee13e07ec3f920c44a6ddefebc2a2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feaad14c8994379ff4ae68ea3b37d24b",
+    "url": "/it-kam-social-network/index.html"
+  },
+  {
+    "revision": "aa67113d09bb4a2c1e53",
+    "url": "/it-kam-social-network/static/css/3.26495e6b.chunk.css"
+  },
+  {
+    "revision": "c93211a2c49ded4b1485",
+    "url": "/it-kam-social-network/static/css/4.4c305e9b.chunk.css"
+  },
+  {
+    "revision": "4fafd556b7f438cd48af",
+    "url": "/it-kam-social-network/static/css/main.68bea6a8.chunk.css"
+  },
+  {
+    "revision": "12dc1cd5269e8a397288",
+    "url": "/it-kam-social-network/static/js/2.211fe4a0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/it-kam-social-network/static/js/2.211fe4a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa67113d09bb4a2c1e53",
+    "url": "/it-kam-social-network/static/js/3.6cd3bc4c.chunk.js"
+  },
+  {
+    "revision": "c93211a2c49ded4b1485",
+    "url": "/it-kam-social-network/static/js/4.f605e795.chunk.js"
+  },
+  {
+    "revision": "4fafd556b7f438cd48af",
+    "url": "/it-kam-social-network/static/js/main.2fd4615c.chunk.js"
+  },
+  {
+    "revision": "fe2e133fe259502e0645",
+    "url": "/it-kam-social-network/static/js/runtime-main.4a50e01e.js"
+  },
+  {
+    "revision": "37776cd579d56ea6ed4124c447c1c6bf",
+    "url": "/it-kam-social-network/static/media/dmitry.37776cd5.webp"
+  }
+]);
